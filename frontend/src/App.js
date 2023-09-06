75e1fa5c81a08142749c6dafd7200a5052e47f3d
Alec <106216445+FreshMozzarella@users.noreply.github.com>
2023-09-06 10:04:42 -0400

    add Products context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
+import './App.css';
+import { useState } from "react";
 import Header from './components/Header'
 import Main from './components/Main'
-import './App.css';
-
+import {ProductConext} from './data/ProductContext'
 function App() {
+  const {Provider: ProductData, Consumer} = ProductConext
+  const [state, setState] = useState({})
   return (
     <div className="App">
+      <ProductData value={{state, setState}}>
       <Header />
       <Main />
+      </ProductData>
     </div>
   );
 }
--- frontend/src/pages/Products/index.jsx
+++ frontend/src/pages/Products/index.jsx
@@ -1,4 +1,19 @@
+import {useState, useEffect} from "react"
 export default function Products(props){
+    const BASE_URL = "http://localhost:4000/products";
+    const [isLoading, setIsLoading] = useState(true);
+    const [products, setProducts] = useState([]);
+
+    const getProducts = async () => {
+        try {
+          const response = await fetch(BASE_URL);
+          const allPeople = await response.json();
+          setPeople(allPeople);
+          setIsLoading(false);
+        } catch (err) {
+          console.log(err);
+        }
+      };
     return (
         <h1>Products</h1>
     )
