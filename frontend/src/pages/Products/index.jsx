8fd3e22218019f690c2d9ac7e85578173feecd73
humayuntariq98 <hammytar23@gmail.com>
2023-09-06 21:11:22 +0500

    add product api and product service modules

--- frontend/src/pages/Products/index.jsx
+++ frontend/src/pages/Products/index.jsx
@@ -1,27 +1,32 @@
 import './Product.css'
 
+import { getProducts } from '../../utilities/product-service';
+
 import {useState, useEffect} from "react"
 export default function Products(props){
     const BASE_URL = "http://localhost:4000/products";
     const [isLoading, setIsLoading] = useState(true);
     const [products, setProducts] = useState([]);
 
-    const getProducts = async () => {
+		async function handleRequest(){
       try {
-        const response = await fetch(BASE_URL);
-        const allProducts = await response.json();
-        setProducts(allProducts);
-        setIsLoading(false);
+        const productsData = await getProducts();
+        if(productsData.length){
+          // console.log(productsData)
+          setProducts(productsData);
+          setIsLoading(false);
+        }else {
+          setIsLoading(false);
+          throw Error(productsData)
+        }
       } catch (err) {
-        console.log(err);
+      console.log(err);
       }
-    };
-  
+    }
 
-
-    useEffect(() => {
-      getProducts();
-    }, []);
+    useEffect(()=>{
+      handleRequest()
+    }, [])
   
     console.log(`There are ${products.length} products available to render`);
 
@@ -32,7 +37,6 @@ export default function Products(props){
             <h3>{p.name}</h3>
             <img className="front-image" src={p.images[0]} alt="front-image" />
             <img className="back-image" src={p.images[1]} alt="back-image" />
-            <p>{p.product_description}</p>
             <p>{p.price}</p>
           </div>
         );
