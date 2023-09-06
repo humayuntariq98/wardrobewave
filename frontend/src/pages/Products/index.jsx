3458773a6c3162d519a12e8bf232d65b2000f9ab
humayuntariq98 <hammytar23@gmail.com>
2023-09-06 20:51:11 +0500

    retreive product data from api

--- frontend/src/pages/Products/index.jsx
+++ frontend/src/pages/Products/index.jsx
@@ -2,26 +2,28 @@ import './Product.css'
 
 import {useState, useEffect} from "react"
 export default function Products(props){
-    const BASE_URL = "http://localhost:3000/products";
+    const BASE_URL = "http://localhost:4000/products";
     const [isLoading, setIsLoading] = useState(true);
     const [products, setProducts] = useState([]);
 
-   async function getProducts (){
-    try {
-      const response = await fetch(BASE_URL);
-      const allProducts = await response.json();
-      setProducts(allProducts);
-      setIsLoading(false);
-    } catch (err) {
-      console.log(err);
-    }
+    const getProducts = async () => {
+      try {
+        const response = await fetch(BASE_URL);
+        const allProducts = await response.json();
+        setProducts(allProducts);
+        setIsLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
     };
+  
+
 
     useEffect(() => {
       getProducts();
     }, []);
   
-    console.log(`There are ${products.length} people available to render`);
+    console.log(`There are ${products.length} products available to render`);
 
     const loaded = () => {
       return products?.map((p) => {
@@ -29,7 +31,7 @@ export default function Products(props){
           <div key={p._id}>
             <h3>{p.name}</h3>
             <img className="front-image" src={p.images[0]} alt="front-image" />
-            <img className="back-image" src={p.images[1]} alt="front-image" />
+            <img className="back-image" src={p.images[1]} alt="back-image" />
             <p>{p.product_description}</p>
             <p>{p.price}</p>
           </div>
