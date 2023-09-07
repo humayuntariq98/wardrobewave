6bd3d19d15d32b14bac83486b72f488aec0ac9a0
Alec <106216445+FreshMozzarella@users.noreply.github.com>
2023-09-07 16:53:09 -0400

    Added card modal for front page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom'
 import Auth0ProviderWithNavigate from "./components/Auth/AuthProvider";
+import { ThemeProvider } from "@material-tailwind/react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
   <React.StrictMode>
+  <ThemeProvider>
     <Auth0ProviderWithNavigate>
     <App />
     </Auth0ProviderWithNavigate>
+    </ThemeProvider>
   </React.StrictMode>
   </Router>
 );
--- frontend/src/pages/Products/index.jsx
+++ frontend/src/pages/Products/index.jsx
@@ -1,69 +1,104 @@
-import './Product.css'
-import {Link} from 'react-router-dom'
+import './Product.css';
+import { Link } from 'react-router-dom';
 import { getProducts } from '../../utilities/product-service';
 import { useAuth0 } from "@auth0/auth0-react";
-import {useState, useEffect} from "react"
-export default function Products(props){
-    const BASE_URL = "http://localhost:4000/product";
-    const [isLoading, setIsLoading] = useState(true);
-    const [products, setProducts] = useState([]);
-    const { isAuthenticated, user } = useAuth0();
-		async function handleRequest(){
-      try {
-        const productsData = await getProducts();
-        if(productsData.length){
-          // console.log(productsData)
-          setProducts(productsData);
-          setIsLoading(false);
-        }else {
-          setIsLoading(false);
-          throw Error(productsData)
-        }
-      } catch (err) {
-      console.log(err);
+import { useState, useEffect } from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+
+export default function Products(props) {
+  const BASE_URL = "http://localhost:4000/product";
+  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+  const { isAuthenticated, user } = useAuth0();
+
+  async function handleRequest() {
+    try {
+      const productsData = await getProducts();
+      if (productsData.length) {
+        setProducts(productsData);
+        setIsLoading(false);
+      } else {
+        setIsLoading(false);
+        throw Error(productsData)
       }
+    } catch (err) {
+      console.log(err);
     }
-
-    useEffect(()=>{
-      handleRequest()
-    }, [])
-  
-    console.log(`There are ${products.length} products available to render`);
-
-    const loaded = () => {
-      return products?.map((p) => {
-        return (
-          <div key={p._id}>
-            <h3>{p.name}</h3>
-            <img className="front-image" src={p.images[0]} alt="front-image" />
-            <img className="back-image" src={p.images[1]} alt="back-image" />
-            <Link to={`/products/${p._id}`}>Details</Link>
-            <p>{p.price}</p>
-          </div>
-        );
-      });
-    };
-  
-    const loading = () => (
-      <div className="product-list">
-        <h1>
-          Loading...
-          <span>
-            <img
-              className="spinner"
-              src="https://freesvg.org/img/1544764567.png"
-            />
-          </span>
-        </h1>
-      </div>
-    );
-  
-    return (
-      <section className="product-list">
-        {isAuthenticated && <h2>Welcome back, {user.name}!</h2>}
-        {isLoading ? loading() : loaded()}
-      </section>
-    );
   }
-  
-   
+
+  useEffect(() => {
+    handleRequest()
+  }, []);
+
+  const EcommerceCard = ({ product }) => {
+    const [showFront, setShowFront] = useState(true);
+
+    return (
+      <Card className="w-96">
+        <CardHeader shadow={false} floated={false} className="h-96 relative">
+          <img
+            onClick={() => setShowFront(!showFront)}
+            src={showFront ? product.images[0] : product.images[1]}
+            alt="card-image"
+            className="h-full w-full object-cover cursor-pointer"
+          />
+          <span className="absolute bottom-4 left-4 bg-white text-gray-800 px-2 py-1 rounded-sm">Click to view</span>
+        </CardHeader>
+        <CardBody>
+          <div className="mb-2 flex items-center justify-between">
+            <Typography color="blue-gray" className="font-medium">
+              {product.name}
+            </Typography>
+            <Typography color="blue-gray" className="font-medium">
+              ${product.price}
+            </Typography>
+          </div>
+        </CardBody>
+        <CardFooter className="pt-0">
+          <Link to={`/products/${product._id}`}>
+            <Button
+              ripple={false}
+              fullWidth={true}
+              className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+            >
+              View Details
+            </Button>
+          </Link>
+        </CardFooter>
+      </Card>
+    );
+  };
+
+  const loaded = () => {
+    return products?.map((p) => {
+      return (
+        <EcommerceCard key={p._id} product={p} />
+      );
+    });
+  };
+
+  const loading = () => (
+    <div className="product-list">
+      <h1>
+        Loading...
+        <span>
+          <img className="spinner" src="https://freesvg.org/img/1544764567.png" />
+        </span>
+      </h1>
+    </div>
+  );
+
+  return (
+    <section className="product-list">
+      {isAuthenticated && <h2>Welcome back, {user.name}!</h2>}
+      {isLoading ? loading() : loaded()}
+    </section>
+  );
+}
