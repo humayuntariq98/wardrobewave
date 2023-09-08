f28c3444ea2a2ad7c106c38ad94cd56fc192f0c1
humayuntariq98 <hammytar23@gmail.com>
2023-09-08 21:21:18 +0500

    add app title and description in readme

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-## WardrobeWave
-
+## Wardrobewave
+"Wardrobe Wave is your ultimate application for seamless and stylish online shopping in the world of fashion. Explore a vast collection of trendsetting clothing, for both men and women all curated to keep you on the forefront of style. <br>
+With Wardrobe Wave, enjoy a user-friendly e-commerce experience, discover the latest fashion trends, and elevate your wardrobe effortlessly. 
 ## Screenshots
 
 ## Technologies Used
--- frontend/src/pages/Show/index.jsx
+++ frontend/src/pages/Show/index.jsx
@@ -2,6 +2,10 @@ import {useState, useEffect} from 'react'
 import { useParams } from "react-router"
 import { getProduct } from "../../utilities/product-service"
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@material-tailwind/react";
+import { Carousel } from "@material-tailwind/react";
+import { Typography } from "@material-tailwind/react";
+import "./showPage.css"
 
 export default function Show(){
     const [product, setProduct] = useState(null)
@@ -35,15 +39,33 @@ export default function Show(){
     useEffect(() => {
         handleRequest();
       }, [])
+      console.log(user,"checking user")
       const loaded = () => (
         <div className="product">
-            {isAuthenticated && <h2>Welcome back, {user.name}!</h2>}
-          <h1>Show Page</h1>
+            {/* {isAuthenticated && <h2>Welcome back, {user.name}!</h2>} */}
+          
+          <div className="carousel-container">
+          <Carousel transition={{ duration: 2 }} className="rounded-xl">
+          <img src={product.images[0]} alt={product.name + " image"} className="h-full w-full object-cover"/>
+          <img src={product.images[1]} alt={product.name + " image"} className="h-full w-full object-cover"/>
+          </Carousel>
+          <Typography>
           <h2>{product.name}</h2>
+          </Typography>
+          <Typography>
           <h2>{product.product_description}</h2>
-          <img src={product.images[0]} alt={product.name + " image"} />
-          <img src={product.images[1]} alt={product.name + " image"} />
-          <button onClick={() => addToCart(product)}>Add to Cart</button>
+          </Typography>
+          <Typography>
+          <h3>${product.price}</h3>
+          </Typography>
+          <Button
+              className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+              ripple="light" 
+              onClick={() => addToCart(product)}
+               >
+               Add to Cart
+            </Button>
+          </div>
         </div>
       );
       
