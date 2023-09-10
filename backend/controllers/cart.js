a11edc5479f1071e346345a649302ce14cd0c752
Humayun Tariq Mughal <138518548+humayuntariq98@users.noreply.github.com>
2023-09-10 01:10:43 -0400

    fixed increment and decrement

--- backend/controllers/cart.js
+++ backend/controllers/cart.js
@@ -1,22 +1,22 @@
 //cart controller
 
-const express = require('express');
-const { Cart } = require('../models');
-const { calculateTotalPriceOfCart} = require( '../utils/utils')
+const express = require("express");
+const { Cart } = require("../models");
+const { calculateTotalPriceOfCart } = require("../utils/utils");
 module.exports = {
   getCart,
   createOrUpdate,
   updateCartItemQuantity,
-  destroy
+  destroy,
 };
 
 async function getCart(req, res, next) {
   try {
-    const userId = req.query.user
-    const cart =await Cart.findOne({userId}) 
+    const userId = req.query.user;
+    const cart = await Cart.findOne({ userId });
     res.json(cart);
   } catch (error) {
-    console.log("error happened",error)
+    console.log("error happened", error);
     //send error
     res.status(400).json(error);
   }
@@ -24,51 +24,51 @@ async function getCart(req, res, next) {
 
 async function createOrUpdate(req, res, next) {
   try {
-    const {userId, product} = req.body;
-    const currentCart = await Cart.findOne({userId})
-    if(currentCart){
-      const isProductAlreadyInCart = currentCart.products.findIndex(p=>p._id === product._id)
-      if(isProductAlreadyInCart!==-1){
+    const { userId, product } = req.body;
+    const currentCart = await Cart.findOne({ userId });
+    if (currentCart) {
+      const isProductAlreadyInCart = currentCart.products.findIndex((p) =>
+        p._id.equals(product._id)
+      );
+      if (isProductAlreadyInCart !== -1) {
         currentCart.products[isProductAlreadyInCart].quantity++;
-      } else{
-        currentCart.products.push({...product, quantity:1})
+      } else {
+        currentCart.products.push({ ...product, quantity: 1 });
       }
       currentCart.totalAmount = calculateTotalPriceOfCart(currentCart);
-      await Cart.findByIdAndUpdate(currentCart._id, currentCart, {new: true})
-      res.json(currentCart)
+      await Cart.findByIdAndUpdate(currentCart._id, currentCart, { new: true });
+      res.json(currentCart);
+    } else {
+      const cartData = {
+        products: [{ ...product, quantity: 1 }],
+        userId,
+        totalAmount: product.price,
+      };
+      const newCart = Cart.create(cartData);
+      res.json(newCart);
     }
-    else{
-
-      const cartData = {products:[{...product, quantity:1}], userId, totalAmount: product.price}
-      const newCart = Cart.create(cartData)
-      res.json(newCart)
-    }
-    
+  } catch (error) {
+    console.log(error);
+  }
 }
-catch(error) {
-  console.log(error)
-}
-}
-
 
 async function updateCartItemQuantity(req, res, next) {
   try {
-    const {productId} = req.params.id
-    const { userId, action } = req.body;
+    const { userId, action, productId } = req.body;
+    console.log("%ccart.js line:58 userId", "color: #007acc;", userId);
+    console.log("%ccart.js line:59 action", "color: #007acc;", action);
+    console.log("%ccart.js line:60 productId", "color: #007acc;", productId);
 
-    console.log('Received userId:', userId);
-    console.log('Received productId:', productId);
-    console.log('Received action:', action);
-    
     const currentCart = await Cart.findOne({ userId });
 
     if (currentCart) {
-      const productIndex = currentCart.products.findIndex(p => p._id === productId);
-
+      const productIndex = currentCart.products.findIndex((p) =>
+        p._id.equals(productId)
+      );
       if (productIndex !== -1) {
-        if (action === 'increment') {
+        if (action === "increment") {
           currentCart.products[productIndex].quantity++;
-        } else if (action === 'decrement') {
+        } else if (action === "decrement") {
           currentCart.products[productIndex].quantity--;
           if (currentCart.products[productIndex].quantity <= 0) {
             // Remove the product if the quantity becomes zero or negative
@@ -77,30 +77,31 @@ async function updateCartItemQuantity(req, res, next) {
         }
 
         currentCart.totalAmount = calculateTotalPriceOfCart(currentCart);
-        await Cart.findByIdAndUpdate(currentCart._id, currentCart, { new: true });
+        await Cart.findByIdAndUpdate(currentCart._id, currentCart, {
+          new: true,
+        });
         res.json(currentCart);
       } else {
-        res.status(404).json({ message: 'Product not found in the cart' });
+        res.status(404).json({ message: "Product not found in the cart" });
       }
     } else {
-      res.status(404).json({ message: 'Cart not found' });
+      res.status(404).json({ message: "Cart not found" });
     }
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 }
 
-async function destroy(req,res,next){
-  try{
+async function destroy(req, res, next) {
+  try {
     const userId = req.params.userId;
-    console.log('user id to delete cart',req.params.id);
-    const cartres = await Cart.findOneAndDelete({userId:req.params.id});
-    console.log('cart deleted',cartres);
-    res.status(200).json()
-  }
-  catch(error){
-    console.log('error',error);
-    res.status(500).json(error)
+    console.log("user id to delete cart", req.params.id);
+    const cartres = await Cart.findOneAndDelete({ userId: req.params.id });
+    console.log("cart deleted", cartres);
+    res.status(200).json();
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json(error);
   }
 }
--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,15 +1,13 @@
-const express = require ('express')
-const router = express.Router()
-const cartCtrl = require('../controllers/cart')
+const express = require("express");
+const router = express.Router();
+const cartCtrl = require("../controllers/cart");
 
 // cart CREATE ROUTE
 
-
 router.get("/", cartCtrl.getCart);
 router.post("/", cartCtrl.createOrUpdate);
-router.delete('/::id',cartCtrl.destroy);
+router.delete("/::id", cartCtrl.destroy);
 
-router.post('/:id/updateQuantity', cartCtrl.updateCartItemQuantity);
+router.post("/updateQuantity", cartCtrl.updateCartItemQuantity);
 
-
-module.exports = router
+module.exports = router;
